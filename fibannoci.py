78dbb05cab6f1a3551d12525a5d55fb17fcdcc1c
deepak90221 <116907628+deepak90221@users.noreply.github.com>
2024-01-12 08:30:06 +0530

    Update fibannoci.py

--- fibannoci.py
+++ fibannoci.py
@@ -1,5 +1,13 @@
-not yet coded..check for others
-:wq
-:wq
-:wq
 
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2 
+count = 1
+
+while count <= n:
+	print(next_number, end=" ")
+	count += 1
+	num1, num2 = num2, next_number
+	next_number = num1 + num2
+print()
